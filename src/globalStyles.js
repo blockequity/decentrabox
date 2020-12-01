51661d8a7e647814fcd928f2fe7a6c887a7a4972
Lloyd Faulk <>
2020-12-01 13:22:26 -0800

    working code solution

---
+++ src/globalStyles.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Montserrat', sans-serif;
+  background: #16151f;
+}
+`;
+
+export default GlobalStyle;
