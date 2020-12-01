0ff69bef0b1ded834c0b6b8a5de5b81e2e046894
Lloyd Faulk <>
2020-12-01 13:27:58 -0800

    Updates

---
+++ precache-manifest.b214ea22f18a46574863ec2da7b33b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ac67712322798004cf7800fe488b51",
+    "url": "/decentrabox/index.html"
+  },
+  {
+    "revision": "a556e057f2b9e1663330",
+    "url": "/decentrabox/static/css/2.3cb1d0e3.chunk.css"
+  },
+  {
+    "revision": "a556e057f2b9e1663330",
+    "url": "/decentrabox/static/js/2.fcc83600.chunk.js"
+  },
+  {
+    "revision": "1017ea88e3dc42aaca503c50647c7b73",
+    "url": "/decentrabox/static/js/2.fcc83600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400a590aecee8f30af2e",
+    "url": "/decentrabox/static/js/main.57b8948d.chunk.js"
+  },
+  {
+    "revision": "4c99b22c62a2d0396ec4",
+    "url": "/decentrabox/static/js/runtime-main.87f50aa5.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/decentrabox/static/media/box.df8b84d5.png"
+  }
+]);
